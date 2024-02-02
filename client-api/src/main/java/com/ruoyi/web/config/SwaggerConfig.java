f12d5caaa22cc9aed2dd2af2ba6b2ee9b2efe746
gazige <gazige@gmail.com>
2024-02-02 20:58:05 +0800

    'scan'

--- client-api/src/main/java/com/ruoyi/web/config/SwaggerConfig.java
+++ client-api/src/main/java/com/ruoyi/web/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.ruoyi.web.config;
 
 import com.ruoyi.common.config.RuoYiConfig;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,22 +24,17 @@ import java.util.List;
  * @author ruoyi
  */
 @Configuration
-public class SwaggerConfig {
-    /**
-     * 系统基础配置
-     */
+public class SwaggerConfig
+{
+    /** 系统基础配置 */
     @Autowired
     private RuoYiConfig ruoyiConfig;
 
-    /**
-     * 是否开启swagger
-     */
+    /** 是否开启swagger */
     @Value("${swagger.enabled}")
     private boolean enabled;
 
-    /**
-     * 设置请求的统一前缀
-     */
+    /** 设置请求的统一前缀 */
     @Value("${swagger.pathMapping}")
     private String pathMapping;
 
@@ -46,12 +42,20 @@ public class SwaggerConfig {
      * 创建API
      */
     @Bean
-    public Docket createRestApi() {
+    public Docket createRestApi()
+    {
         return new Docket(DocumentationType.OAS_30)
+                // 是否启用Swagger
                 .enable(enabled)
+                // 用来创建该API的基本信息，展示在文档的页面中（自定义展示的信息）
                 .apiInfo(apiInfo())
+                // 设置哪些接口暴露给Swagger展示
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ruoyi.web.controller"))
+                // 扫描所有有注解的api，用这种方式更灵活
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                // 扫描指定包中的swagger注解
+                 .apis(RequestHandlerSelectors.basePackage("com.ruoyi.web.controller"))
+//                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
                 /* 设置安全模式，swagger可以设置访问token */
@@ -63,7 +67,8 @@ public class SwaggerConfig {
     /**
      * 安全模式，这里指定token通过Authorization头请求头传递
      */
-    private List<SecurityScheme> securitySchemes() {
+    private List<SecurityScheme> securitySchemes()
+    {
         List<SecurityScheme> apiKeyList = new ArrayList<SecurityScheme>();
         apiKeyList.add(new ApiKey("Authorization", "Authorization", In.HEADER.toValue()));
         return apiKeyList;
@@ -72,7 +77,8 @@ public class SwaggerConfig {
     /**
      * 安全上下文
      */
-    private List<SecurityContext> securityContexts() {
+    private List<SecurityContext> securityContexts()
+    {
         List<SecurityContext> securityContexts = new ArrayList<>();
         securityContexts.add(
                 SecurityContext.builder()
@@ -85,7 +91,8 @@ public class SwaggerConfig {
     /**
      * 默认的安全上引用
      */
-    private List<SecurityReference> defaultAuth() {
+    private List<SecurityReference> defaultAuth()
+    {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
@@ -97,7 +104,8 @@ public class SwaggerConfig {
     /**
      * 添加摘要信息
      */
-    private ApiInfo apiInfo() {
+    private ApiInfo apiInfo()
+    {
         // 用ApiInfoBuilder进行定制
         return new ApiInfoBuilder()
                 // 设置标题
