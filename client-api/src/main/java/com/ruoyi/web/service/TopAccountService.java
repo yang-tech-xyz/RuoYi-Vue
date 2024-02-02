9a454b3b4046e172cd53c143a441c188b0028af1
gazige <gazige@gmail.com>
2024-02-02 22:46:09 +0800

    'scan'

--- client-api/src/main/java/com/ruoyi/web/service/TopAccountService.java
+++ client-api/src/main/java/com/ruoyi/web/service/TopAccountService.java
@@ -1,14 +1,68 @@
 package com.ruoyi.web.service;
 
+import cn.hutool.core.lang.UUID;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ruoyi.web.dto.AccountRequest;
+import com.ruoyi.web.dto.AccountTxRequest;
+import com.ruoyi.web.dto.AccountTxRequestDetail;
 import com.ruoyi.web.entity.TopAccount;
 import com.ruoyi.web.mapper.TopAccountMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class TopAccountService extends ServiceImpl<TopAccountMapper, TopAccount> {
 
+    @Autowired
+    private TopAccountTxService txService;
 
+    public void processAccount(List<AccountRequest> requests) {
+        log.info("【TOP - API】 -> 资产处理:{}", requests);
+        AccountTxRequest txRequest = new AccountTxRequest();
+        txRequest.setTxNo(UUID.fastUUID().toString(true));
+        requests.stream().filter(e -> e.getBalanceChanged().compareTo(BigDecimal.ZERO) != 0).forEach(e -> {
+            txRequest.addDetail(AccountTxRequestDetail.builder()
+                    .uniqueId(e.getUniqueId())
+                    .accountId(getAccount(e.getMebId(), e.getSymbol()).getId())
+                    .mebId(e.getMebId())
+                    .symbol(e.getSymbol())
+                    .fee(e.getFee())
+                    .balanceChanged(e.getBalanceChanged())
+                    .balanceTxType(e.getBalanceTxType())
+                    .txType(e.getTxType())
+                    .refNo(e.getRefNo())
+                    .remark(e.getRemark()).build());
+        });
+        if (!txRequest.getDetails().isEmpty()) {
+            txService.processedTx(txRequest);
+        }
+    }
+
+    public TopAccount getAccount(Long mebId, String symbol) {
+        Optional<TopAccount> optional = Optional.ofNullable(baseMapper.selectOne(new LambdaQueryWrapper<TopAccount>()
+                .eq(TopAccount::getMebId, mebId).eq(TopAccount::getSymbol, symbol)));
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        TopAccount account = new TopAccount();
+        account.setMebId(mebId);
+        account.setSymbol(symbol);
+        account.setAvailableBalance(BigDecimal.ZERO);
+        account.setLockupBalance(BigDecimal.ZERO);
+        account.setFrozenBalance(BigDecimal.ZERO);
+        account.setCreatedDate(LocalDateTime.now());
+        account.setCreatedBy(String.valueOf(mebId));
+        account.setUpdatedDate(LocalDateTime.now());
+        account.setUpdatedBy(String.valueOf(mebId));
+        baseMapper.insert(account);
+        return account;
+    }
 }
--- client-api/src/main/java/com/ruoyi/web/service/TopAccountTxService.java
+++ client-api/src/main/java/com/ruoyi/web/service/TopAccountTxService.java
@@ -1,13 +1,84 @@
 package com.ruoyi.web.service;
 
+import cn.hutool.core.lang.Assert;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ruoyi.web.dto.AccountTxRequest;
+import com.ruoyi.web.dto.AccountTxRequestDetail;
+import com.ruoyi.web.entity.TopAccount;
 import com.ruoyi.web.entity.TopAccountTx;
+import com.ruoyi.web.enums.Account;
+import com.ruoyi.web.exception.ServiceException;
+import com.ruoyi.web.mapper.TopAccountMapper;
 import com.ruoyi.web.mapper.TopAccountTxMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class TopAccountTxService extends ServiceImpl<TopAccountTxMapper, TopAccountTx> {
 
+    @Autowired
+    private TopAccountMapper accountMapper;
+
+    @Transactional(rollbackFor = Exception.class)
+    public void processedTx(AccountTxRequest txRequest) {
+        log.info("【TOP - API】 ->  资产处理,txRequest：{}", txRequest);
+        Map<Long, List<AccountTxRequestDetail>> txMap = txRequest.getDetails().stream().collect(Collectors.groupingBy(AccountTxRequestDetail::getAccountId));
+        txMap.forEach((accountId, accounts) -> {
+            TopAccount lockAccount = accountMapper.lockById(accountId);
+            Assert.notNull(lockAccount, "invalid accountId");
+            accounts.forEach(a -> {
+                TopAccountTx tx = new TopAccountTx();
+                if (StringUtils.equals(a.getBalanceTxType().typeCode, Account.Balance.AVAILABLE.typeCode)) {
+                    tx.setBalanceBefore(lockAccount.getAvailableBalance());
+                    lockAccount.setAvailableBalance(lockAccount.getAvailableBalance().add(a.getBalanceChanged()).add(a.getFee()));
+                    tx.setBalanceAfter(lockAccount.getAvailableBalance());
+                }
+                if (StringUtils.equals(a.getBalanceTxType().typeCode, Account.Balance.FROZEN.typeCode)) {
+                    tx.setBalanceBefore(lockAccount.getFrozenBalance());
+                    lockAccount.setFrozenBalance(lockAccount.getFrozenBalance().add(a.getBalanceChanged()).add(a.getFee()));
+                    tx.setBalanceAfter(lockAccount.getFrozenBalance());
+                }
+                if (StringUtils.equals(a.getBalanceTxType().typeCode, Account.Balance.LOCKUP.typeCode)) {
+                    tx.setBalanceBefore(lockAccount.getLockupBalance());
+                    lockAccount.setLockupBalance(lockAccount.getLockupBalance().add(a.getBalanceChanged()).add(a.getFee()));
+                    tx.setBalanceAfter(lockAccount.getLockupBalance());
+                }
+                tx.setAccountId(accountId);
+                tx.setMebId(a.getMebId());
+                tx.setSymbol(lockAccount.getSymbol());
+                tx.setUniqueId(a.getUniqueId());
+                tx.setAmount(a.getBalanceChanged());
+                tx.setFee(a.getFee());
+                tx.setTransactionNo(txRequest.getTxNo());
+                tx.setBalanceType(a.getBalanceTxType().typeCode);
+                tx.setTxType(a.getTxType().typeCode);
+                tx.setRefNo(a.getRefNo());
+                tx.setRemark(a.getRemark());
+                tx.setCreatedBy(String.valueOf(tx.getMebId()));
+                tx.setCreatedDate(LocalDateTime.now());
+                tx.setUpdatedBy(String.valueOf(tx.getMebId()));
+                tx.setUpdatedDate(LocalDateTime.now());
+                baseMapper.insert(tx);
+            });
+            checkBalance(lockAccount.getAvailableBalance(), lockAccount.getFrozenBalance(), lockAccount.getLockupBalance());
+            accountMapper.updateById(lockAccount);
+        });
+    }
+
+    public void checkBalance(BigDecimal... balance) {
+        if (Arrays.stream(balance).anyMatch(e -> e.compareTo(BigDecimal.ZERO) < 0)) {
+            throw new ServiceException("余额不足", 500);
+        }
+    }
 }
