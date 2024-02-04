f560fb368c2886e33c1f1744bd1bf0cfc22d394f
gazige <gazige@gmail.com>
2024-02-04 20:25:41 +0800

    's'

--- client-api/src/main/java/com/ruoyi/web/ClientApiApplication.java
+++ client-api/src/main/java/com/ruoyi/web/ClientApiApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
