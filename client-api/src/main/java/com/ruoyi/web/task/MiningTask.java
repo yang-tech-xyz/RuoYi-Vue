9e57167a98adebef20f8963a4b0f6830fb579213
gazige <gazige@gmail.com>
2024-02-06 16:37:09 +0800

    's'

--- client-api/src/main/java/com/ruoyi/web/task/MiningTask.java
+++ client-api/src/main/java/com/ruoyi/web/task/MiningTask.java
@@ -1,15 +1,11 @@
 package com.ruoyi.web.task;
 
-import com.ruoyi.web.service.TopTokenPriceService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MiningTask {
 
-    @Autowired
-    private TopTokenPriceService service;
 
     /**
      * 矿池挖矿
@@ -18,8 +14,7 @@ public class MiningTask {
      * 3.发放对应的BTC换算USDT
      */
     @Scheduled(initialDelay = 1000, fixedDelay = 3000)
-    public void refPrice() {
-        service.refPrice();
+    public void mining() {
     }
 
 }
