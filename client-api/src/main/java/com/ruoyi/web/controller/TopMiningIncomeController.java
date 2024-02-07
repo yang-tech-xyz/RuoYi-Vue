28bfa7147bb6caece8659112255c50b3d8463320
gazige <gazige@gmail.com>
2024-02-07 14:04:37 +0800

    's'

--- client-api/src/main/java/com/ruoyi/web/controller/TopMiningIncomeController.java
+++ client-api/src/main/java/com/ruoyi/web/controller/TopMiningIncomeController.java
@@ -1,12 +1,12 @@
 package com.ruoyi.web.controller;
 
 import com.ruoyi.web.service.TopMiningIncomeService;
-import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "挖矿收益", value = "挖矿收益")
+@Tag(description = "挖矿收益", name = "挖矿收益")
 @RestController
 @RequestMapping("topMiningIncome")
 public class TopMiningIncomeController {
