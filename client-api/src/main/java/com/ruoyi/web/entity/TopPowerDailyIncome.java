02c538fe1022f1eef289078eb0b3b8fe7c38ee98
gazige <gazige@gmail.com>
2024-02-08 07:53:33 +0800

    's'

--- client-api/src/main/java/com/ruoyi/web/entity/TopPowerDailyIncome.java
+++ client-api/src/main/java/com/ruoyi/web/entity/TopPowerDailyIncome.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import io.swagger.annotations.ApiModelProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,43 +20,43 @@ import java.time.LocalDateTime;
 @TableName(value = "top_power_daily_income")
 public class TopPowerDailyIncome implements Serializable {
 
-    @ApiModelProperty("主键")
+    @Schema(description = "主键")
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
-    @ApiModelProperty("用户ID")
+    @Schema(description = "用户ID")
     @TableField(value = "user_id")
     private Long userId;
 
-    @ApiModelProperty("订单号")
+    @Schema(description = "订单号")
     @TableField(value = "order_no")
     private String orderNo;
 
-    @ApiModelProperty("存入金额")
+    @Schema(description = "存入金额")
     @TableField(value = "amount")
     private BigDecimal amount;
 
-    @ApiModelProperty("收益币种")
+    @Schema(description = "收益币种")
     @TableField(value = "income_symbol")
     private String incomeSymbol;
 
-    @ApiModelProperty("币种价格")
+    @Schema(description = "币种价格")
     @TableField(value = "income_price")
     private BigDecimal incomePrice;
 
-    @ApiModelProperty("比列")
+    @Schema(description = "比列")
     @TableField(value = "income_rate")
     private BigDecimal incomeRate;
 
-    @ApiModelProperty("收益金额USD")
+    @Schema(description = "收益金额USD")
     @TableField(value = "income_usd")
     private BigDecimal incomeUsd;
 
-    @ApiModelProperty("币种收益金额")
+    @Schema(description = "币种收益金额")
     @TableField(value = "income")
     private BigDecimal income;
 
-    @ApiModelProperty("收益日期")
+    @Schema(description = "收益日期")
     @TableField(value = "income_date")
     private LocalDate incomeDate;
 
@@ -66,19 +65,19 @@ public class TopPowerDailyIncome implements Serializable {
     private Boolean processEnabled;
 
 
-    @ApiModelProperty("创建日期")
+    @Schema(description = "创建日期")
     @TableField(value = "created_date")
     private LocalDateTime createdDate;
 
-    @ApiModelProperty("创建人")
+    @Schema(description = "创建人")
     @TableField(value = "created_by")
     private String createdBy;
 
-    @ApiModelProperty("更新日期")
+    @Schema(description = "更新日期")
     @TableField(value = "updated_date")
     private LocalDateTime updatedDate;
 
-    @ApiModelProperty("更新人")
+    @Schema(description = "更新人")
     @TableField(value = "updated_by")
     private String updatedBy;
 
--- client-api/src/main/java/com/ruoyi/web/entity/TopPowerSharingConfig.java
+++ client-api/src/main/java/com/ruoyi/web/entity/TopPowerSharingConfig.java
@@ -1,50 +1,52 @@
 package com.ruoyi.web.entity;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.*;
-import java.math.BigDecimal;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
-import lombok.*;
-import com.baomidou.mybatisplus.annotation.*;
-import io.swagger.annotations.ApiModelProperty;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @TableName(value = "top_power_sharing_config")
 public class TopPowerSharingConfig implements Serializable {
-    
-    @ApiModelProperty("主键")
+
+    @Schema(description = "主键")
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
-    @ApiModelProperty("层级")
+    @Schema(description = "层级")
     @TableField(value = "level")
     private Integer level;
-    
-    @ApiModelProperty("利率")
+
+    @Schema(description = "利率")
     @TableField(value = "rate")
     private BigDecimal rate;
-    
-    @ApiModelProperty("创建时间")
+
+    @Schema(description = "创建时间")
     @TableField(value = "created_date")
     private LocalDateTime createdDate;
-    
-    @ApiModelProperty("创建人")
+
+    @Schema(description = "创建人")
     @TableField(value = "created_by")
     private String createdBy;
-    
-    @ApiModelProperty("更新时间")
+
+    @Schema(description = "更新时间")
     @TableField(value = "updated_date")
     private LocalDateTime updatedDate;
-    
-    @ApiModelProperty("更新人")
+
+    @Schema(description = "更新人")
     @TableField(value = "updated_by")
     private String updatedBy;
-    
+
 
 }
 
