b9539632fed034bbb0f71a0892fe36095852f6f7
gazige <gazige@gmail.com>
2024-02-08 16:55:49 +0800

    's'

--- client-api/src/main/java/com/ruoyi/web/vo/StoreVO.java
+++ client-api/src/main/java/com/ruoyi/web/vo/StoreVO.java
@@ -8,28 +8,28 @@ import java.math.BigDecimal;
 @Data
 public class StoreVO {
 
-    @Schema(description="主键")
+    @Schema(description = "主键")
     private Long id;
 
-    @Schema(description="产品名称")
+    @Schema(description = "产品名称")
     private String name;
 
-    @Schema(description="产品周期")
+    @Schema(description = "产品周期（月）,每月限定30天")
     private Integer period;
 
-    @Schema(description="存入币种")
-    private String token;
+    @Schema(description = "存入币种")
+    private String symbol;
 
-    @Schema(description="最小投资额")
+    @Schema(description = "收益币种")
+    private String incomeSymbol;
+
+    @Schema(description = "最小投资额")
     private BigDecimal minOrderAmount;
 
-    @Schema(description="最大投注额")
+    @Schema(description = "最大投注额")
     private BigDecimal maxOrderAmount;
 
-    @Schema(description="最小收益利率")
-    private BigDecimal minRate;
-
-    @Schema(description="最大收益利率")
-    private BigDecimal maxRate;
+    @Schema(description = "收益利率")
+    private BigDecimal rate;
 
 }
