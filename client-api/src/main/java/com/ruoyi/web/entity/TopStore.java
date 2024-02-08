5e5e3f7ebb871c9264c92ca08ec654c43cbcdc67
gazige <gazige@gmail.com>
2024-02-08 16:58:27 +0800

    's'

--- client-api/src/main/java/com/ruoyi/web/entity/TopStore.java
+++ client-api/src/main/java/com/ruoyi/web/entity/TopStore.java
@@ -1,74 +1,76 @@
 package com.ruoyi.web.entity;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.*;
-import java.math.BigDecimal;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
-import lombok.*;
-import com.baomidou.mybatisplus.annotation.*;
-import io.swagger.annotations.ApiModelProperty;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @TableName(value = "top_store")
 public class TopStore implements Serializable {
-    
-    @ApiModelProperty("主键")
+
+    @Schema(description = "主键")
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
-    @ApiModelProperty("产品名称")
+    @Schema(description = "产品名称")
     @TableField(value = "name")
     private String name;
-    
-    @ApiModelProperty("产品周期（月）,每月限定30天")
+
+    @Schema(description = "产品周期（月）,每月限定30天")
     @TableField(value = "period")
     private Integer period;
-    
-    @ApiModelProperty("存入币种")
+
+    @Schema(description = "存入币种")
     @TableField(value = "symbol")
     private String symbol;
-    
-    @ApiModelProperty("收益币种")
+
+    @Schema(description = "收益币种")
     @TableField(value = "income_symbol")
     private String incomeSymbol;
-    
-    @ApiModelProperty("最小投资额")
+
+    @Schema(description = "最小投资额")
     @TableField(value = "min_order_amount")
     private BigDecimal minOrderAmount;
-    
-    @ApiModelProperty("最大投注额")
+
+    @Schema(description = "最大投注额")
     @TableField(value = "max_order_amount")
     private BigDecimal maxOrderAmount;
-    
-    @ApiModelProperty("收益利率")
+
+    @Schema(description = "收益利率")
     @TableField(value = "rate")
     private BigDecimal rate;
-    
-    @ApiModelProperty("状态：1=有效，2=无效")
+
+    @Schema(description = "状态：1=有效，2=无效")
     @TableField(value = "status")
     private Integer status;
-    
-    @ApiModelProperty("创建日期")
+
+    @Schema(description = "创建日期")
     @TableField(value = "created_date")
     private LocalDateTime createdDate;
-    
-    @ApiModelProperty("创建人")
+
+    @Schema(description = "创建人")
     @TableField(value = "created_by")
     private String createdBy;
-    
-    @ApiModelProperty("更新日期")
+
+    @Schema(description = "更新日期")
     @TableField(value = "updated_date")
     private LocalDateTime updatedDate;
-    
-    @ApiModelProperty("更新人")
+
+    @Schema(description = "更新人")
     @TableField(value = "updated_by")
     private String updatedBy;
-    
+
 
 }
 
