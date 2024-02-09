cc142631df56ec0bdc66904f5a9d68463de715a2
gazige <gazige@gmail.com>
2024-02-09 12:14:46 +0800

    's'

--- client-api/src/main/java/com/ruoyi/web/vo/WalletRegisterBody.java
+++ client-api/src/main/java/com/ruoyi/web/vo/WalletRegisterBody.java
@@ -5,18 +5,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 /**
- *
  * 用户钱包注册信息
+ *
  * @author ruoyi
  */
 @Data
-public class WalletRegisterBody extends SignBaseEntity
-{
+public class WalletRegisterBody extends SignBaseEntity {
 
     /**
      * 邀请码
      */
-    @Schema(description = = "邀请码",example = "111")
+    @Schema(description = "邀请码", example = "111")
     private String invitedCode;
 
 }
