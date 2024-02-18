909132c44704275ee43f0be835f92438d6d0e9df
kaiweicai <kaiweicai@163.com>
2024-02-18 20:35:58 +0800

    add chain update

---
+++ client-admin/src/main/java/com/ruoyi/web/controller/TopChainController.java
@@ -0,0 +1,66 @@
+package com.ruoyi.web.controller;
+
+import com.ruoyi.common.AjaxResult;
+import com.ruoyi.web.dto.TopChainDTO;
+import com.ruoyi.web.entity.TopChain;
+import com.ruoyi.web.service.TopChainService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 充值
+ *
+ * @author ruoyi
+ */
+@Slf4j
+@RequestMapping("/chain")
+@Tag(description = "TopChainController", name = "链信息")
+@RestController
+public class TopChainController {
+
+    @Autowired
+    private TopChainService topChainService;
+
+
+    @Operation(summary = "查询当前所有币种")
+    @GetMapping("/getList")
+    public AjaxResult<List<TopChain>> getList() {
+        return AjaxResult.success(topChainService.list());
+    }
+
+    @Operation(summary = "新增chain")
+    @PostMapping("/")
+    public AjaxResult<String> add(TopChainDTO topChainDTO){
+        TopChain topChain = new TopChain();
+        BeanUtils.copyProperties(topChainDTO,topChain);
+        topChain.setCreateTime(LocalDateTime.now());
+        topChain.setUpdateTime(LocalDateTime.now());
+        topChain.setCreateBy("sys");
+        topChain.setUpdateBy("sys");
+        topChainService.save(topChain);
+        return AjaxResult.success();
+    }
+
+    @Operation(summary = "修改链信息")
+    @PutMapping("/")
+    public AjaxResult<String> edit(TopChain topChain){
+        topChain.setUpdateTime(LocalDateTime.now());
+        topChainService.updateById(topChain);
+        return AjaxResult.success();
+    }
+
+    @Operation(summary = "删除链信息")
+    @DeleteMapping("/{id}")
+    public AjaxResult<String> remove(@PathVariable Long id){
+        topChainService.removeById(id);
+        return AjaxResult.success();
+    }
+
+}
